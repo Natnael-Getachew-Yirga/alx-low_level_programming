49416e909d7d347f69cc72dd6293c0096b6dab95
Nati <nowayha8866@gmail.com>
2022-04-12 14:53:42 +0300

    tasK

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print 
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+char l;
+
+for (int i = 0 ; i <=10 ; i++)
+{
+for (l = 'a'; l <= 'z'; l++)
+{
+_putchar(l);
+}
+_putchar('\n');
+}
+}
