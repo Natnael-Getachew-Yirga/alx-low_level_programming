d6c485223edf3263eeb6be1a75846a4b301d3f04
Natnael <younati19th@gmail.com>
2022-04-12 03:19:36 -0700

    task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "main.h"
 /**
  * main - print
@@ -9,11 +8,3 @@ int main(void)
 _putchar("_putchar");
 _putchar('\n');
 return (0);
-}
-<<<<<<< HEAD
-
-=======
->>>>>>> 14c096a5cb8d98aa5542501a76693c3738510d18
-=======
-
->>>>>>> b6d030cae899de50cdd8303685add88b34e8d29a
