3c874a6e6e55ba56d867cfc347a5b74a0bd98a3f
Nati <nowayha8866@gmail.com>
2022-04-12 14:14:17 +0300

    task

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,20 +3,13 @@
  * main - check the code
  * Return: Always 0.
  */
-void print_alphabet(void);
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
 void print_alphabet(void)
 {
-	char l;
+char l;
 
-	for(l='a';l<='z';l++)
-	{
-		_putchar(l);
-		_putchar('\n');
-	}
-
+for(l='a';l<='z';l++)
+{
+_putchar(l);
+}
+_putchar('\n');
 }
